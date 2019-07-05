baa01f5faa243fee20731c24ce33233963f59c61
student <student@192.168.2.19>
2019-07-05 20:41:28 +0800

    SpringMVC

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/control")
@@ -9,6 +10,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         System.out.println("Hello World");
+        return "hello";
+    }
+    
+    @RequestMapping(value = "/hello2", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello2() {
+        System.out.println("Hello World 2");
         return "hello";
     }
 }
