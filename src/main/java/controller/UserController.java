67418cba9f1aae95c5f0b44e7f3190adb8997cc9
student <student@192.168.2.19>
2019-07-08 21:37:01 +0800

    SpringMVC

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +12,20 @@ import vo.User;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private static List<User> list = new ArrayList<>();
     
     // http://localhost:8080/SpringMVC_Web/mvc/user/
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String get() {
-        return "get";
+        return "get:" + list;
     }
     
     // http://localhost:8080/SpringMVC_Web/mvc/user/
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @ResponseBody
     public String post(User user) {
+        list.add(user);
         return "post:" + user;
     }
     
@@ -29,6 +33,13 @@ public class UserController {
     @RequestMapping(value = "/", method = RequestMethod.PUT)
     @ResponseBody
     public String put(User user) {
+        int id = user.getNum().getId();
+        list.stream()
+                .filter(u -> u.getNum().getId() == id)
+                .forEach(u -> {
+                    u.setUname(user.getUname());
+                    u.setUage(user.getUage());
+                });
         return "put:" + user;
     }
     
@@ -36,6 +47,9 @@ public class UserController {
     @RequestMapping(value = "/", method = RequestMethod.DELETE)
     @ResponseBody
     public String delete(User user) {
+        int id = user.getNum().getId();
+        User del_user = list.stream().filter(u -> u.getNum().getId() == id).findFirst().get();
+        list.remove(del_user);
         return "delete:" + user;
     }
     
