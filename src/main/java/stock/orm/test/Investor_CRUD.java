20093e12e3d92052cda8c0803fcfc0c05bc3eae2
student <student@192.168.2.19>
2019-07-12 18:50:13 +0800

    SpringMVC

---
+++ src/main/java/stock/orm/test/Investor_CRUD.java
@@ -0,0 +1,44 @@
+package stock.orm.test;
+
+
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import stock.orm.dao.IFundDao;
+import stock.orm.dao.IInvestorDao;
+import stock.orm.model.Fund;
+import stock.orm.model.Investor;
+import stock.orm.model.Trader;
+
+public class Investor_CRUD {
+    private static ApplicationContext context;
+    private static IInvestorDao investorDao;
+    private static IFundDao fundDao;
+    static {
+        context = new ClassPathXmlApplicationContext("applicationContext_stock.xml");
+        investorDao = (IInvestorDao)context.getBean("investorDao");
+        fundDao = (IFundDao)context.getBean("fundDao");
+    }
+    public static void main(String[] args) {
+        add("Lucky", 10000, "A");
+//        queryAll().stream()
+//                .forEach(e -> {
+//                    System.out.printf("%s %.0f %d %s\n", e.getName(), e.getUnits(), e.getNetValue(), e.getFund().getName());
+//                });
+    }
+    
+    public static void add(String investName, double units, String fundName) {
+        Fund fund = fundDao.find(fundName);
+        Investor inv = new Investor(investName);
+        inv.setNetValue(fund.getFundNet().getValue());
+        inv.setUnits(units);
+        inv.setFund(fund);
+        investorDao.create(inv);
+    }
+    
+    
+    public static List<Investor> queryAll() {
+        return investorDao.queryAll(Investor.class);
+    }
+    
+}
---
+++ src/main/java/stock/orm/test/Trader_CRUD.java
@@ -0,0 +1,37 @@
+package stock.orm.test;
+
+
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import stock.orm.dao.ITraderDao;
+import stock.orm.model.Trader;
+
+public class Trader_CRUD {
+    static ApplicationContext context;
+    static ITraderDao traderDao;
+    static {
+        context = new ClassPathXmlApplicationContext("applicationContext_stock.xml");
+        traderDao = (ITraderDao)context.getBean("traderDao");
+    }
+    public static void main(String[] args) {
+        //add();
+        queryAll().stream()
+                .filter(e -> e.getName().length() >= 4)
+                .forEach(e -> System.out.println(e.getName()));
+    }
+    
+    public static void add() {
+        Trader t1 = new Trader("John");
+        Trader t2 = new Trader("Mary");
+        Trader t3 = new Trader("Tom");
+        traderDao.create(t1);
+        traderDao.create(t2);
+        traderDao.create(t3);
+    }
+    
+    public static List<Trader> queryAll() {
+        return traderDao.queryAll(Trader.class);
+    }
+    
+}
